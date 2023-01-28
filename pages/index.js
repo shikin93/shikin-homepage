053316a37abc86631407b9d7f116fe5513f7807c
shikin93 <official.shiin@gmail.com>
2023-01-28 15:40:13 +0700

    add hero section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <>
-    <div className="text-4xl">Hello world</div>
+      <section className="my-12">
+        <h2 className="font-bold text-6xl mb-3">Chosiin</h2>
+        <p className="text-lg text-accentDark mb-12">I specialize in building web applications using the React JS framework. Developing user interfaces fast, responsive, and easy to use.</p>
+        <Link href="/about">
+          <button className="text-lg border-2 border-accentDark px-4 py-1 rounded-lg duration-500 hover:bg-accentDark hover:text-background">Get in Touch &rarr;</button>
+        </Link>
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default Home;
